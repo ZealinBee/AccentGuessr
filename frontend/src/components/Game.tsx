09d7b2cbd8f5ffb5385da75a19ca349f0543d07a
Zhiyuan Liu <thenukezealot@gmail.com>
2025-10-27 14:22:17 +0200

    netlify comp error fixed

--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -3,16 +3,14 @@ import EndScreen from "./EndScreen";
 import { useGame } from "../hooks/useGame";
 
 function Game() {
-  const {isShowingEndScreen, totalScore, gameData, gameRound} = useGame();
+  const { isShowingEndScreen, totalScore, gameData, gameRound } = useGame();
 
   return (
     <div>
-      {isShowingEndScreen  ? (
-        <EndScreen totalScore={totalScore} />
-      ) : (
-        <Map
-          roundData={gameData[gameRound]}
-        />
+      {isShowingEndScreen && <EndScreen totalScore={totalScore} />}
+
+      {!isShowingEndScreen && gameData && (
+        <Map roundData={gameData[gameRound]} />
       )}
     </div>
   );
